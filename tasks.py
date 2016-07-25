f9ac42f1f5d05ccc36954e12a5df5cc9f52991aa
MinchinWeb <w_minchin@hotmail.com>
2016-07-25 17:13:15 -0600

    Update task file for Invoke 0.13

--- tasks.py
+++ tasks.py
@@ -18,11 +18,13 @@ from pathlib import Path
 #env.cloudfiles_api_key = 'my_rackspace_api_key'
 #env.cloudfiles_container = 'my_cloudfiles_container'
 
+INVOKE_SHELL = 'C:\Windows\system32\cmd.EXE'
+
 p = Path.cwd()
 deploy_path = p.parents[0] / 'minchinweb.github.io-master'
 
 
-def clean():
+def clean(ctx):
     print("You'll have to manually delete the output folder")
     #if os.path.isdir(DEPLOY_PATH):
     #    local('rm -rf {deploy_path}'.format(**env))
@@ -30,73 +32,73 @@ def clean():
 
 
 @task
-def build():
-    run('pelican -s pelicanconf.py')
+def build(ctx):
+    run('pelican -s pelicanconf.py', shell=INVOKE_SHELL)
 
 
 @task
-def rebuild():
-    clean()
-    build()
+def rebuild(ctx):
+    clean(ctx)
+    build(ctx)
 
 
 @task
-def regenerate():
-    run('start pelican -r -s pelicanconf.py')
+def regenerate(ctx):
+    run('start pelican -r -s pelicanconf.py', shell=INVOKE_SHELL)
 
 
 @task
-def serve():
+def serve(ctx):
     # local('cd {deploy_path} && start python -m SimpleHTTPServer'.format(**env))
     # in Python3000, use  python -m http.server
-    run('cd {} && start python -m http.server'.format(deploy_path))
+    run('cd {} && start python -m http.server'.format(deploy_path), shell=INVOKE_SHELL)
 
 
 @task
-def serve_on(port):
+def serve_on(ctx, port):
     # local('cd {deploy_path} && start python -m SimpleHTTPServer'.format(**env))
     # in Python3000, use  python -m http.server
-    run('cd {} && start python -m http.server {}'.format(deploy_path, port))
+    run('cd {} && start python -m http.server {}'.format(deploy_path, port), shell=INVOKE_SHELL)
 
 
 @task
-def reserve():
-    build()
-    serve()
+def reserve(ctx):
+    build(ctx)
+    serve(ctx)
 
 
 @task
-def preview():
-    run('pelican -s publishconf.py')
+def preview(ctx):
+    run('pelican -s publishconf.py', shell=INVOKE_SHELL)
 
 
 @task
-def upload():
-    publish()
-    run('cd {deploy_path}')
-    run('git add -A')
-    run('git commit')
-    run('git push')
+def upload(ctx):
+    publish(ctx)
+    run('cd {deploy_path}', shell=INVOKE_SHELL)
+    run('git add -A', shell=INVOKE_SHELL)
+    run('git commit', shell=INVOKE_SHELL)
+    run('git push', shell=INVOKE_SHELL)
 
 
 @task
-def publish():
-    run('pelican -s publishconf.py')
+def publish(ctx):
+    run('pelican -s publishconf.py', shell=INVOKE_SHELL)
 
 
 # Add devsever
 # only works on Windows
 #  need to kill the second window manually
 @task
-def devserver():
-    regenerate()
-    serve()
+def devserver(ctx):
+    regenerate(ctx)
+    serve(ctx)
 
 @task
-def less():
+def less(ctx):
     #run('lessc theme\\burst-energy\\less\\bootstrap.burst-energy.less > ' +
     #    env_deploy_path + '\\css\\style.css')
     #   lessc themes\pelican-minchin-ca\static\less\bootstrap.minchin-ca.min.less > themes\pelican-minchin-ca\static\css\bootstrap.minchin-ca.min.css
     source = p / 'themes\pelican-minchin-ca\static\less\\bootstrap.minchin-ca.min.less'
     dest = p / 'themes\pelican-minchin-ca\static\css\\bootstrap.minchin-ca.min.css'
-    run('lessc {} > {}'.format(source, dest))
+    run('lessc {} > {}'.format(source, dest), shell=INVOKE_SHELL)
