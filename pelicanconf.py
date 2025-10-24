1f3a7409ccba0d994225b2545c4a9492c69ea0f7
MinchinWeb <w_minchin@hotmail.com>
2025-10-24 14:41:48 -0600

    Revert "[CSCN] add to sidebar"
    This reverts commit 7b88ed93f2137544a4b2310d91c7fe05927d246d.

--- pelicanconf.py
+++ pelicanconf.py
@@ -84,7 +84,6 @@ PATH = "content"
 MENUITEMS = (
     ("Blog", "http://blog.minchin.ca/", "fa fa-pencil"),
     ("Genealogy", "https://genealogy.minchin.ca/", "glyphicon glyphicon-tree-deciduous"),
-    ("Pour Nos Enfants", "https://cscn.minchin.ca/", "fa fa-fw fa-school"),
 )
 DISPLAY_PAGES_ON_MENU = True
 
